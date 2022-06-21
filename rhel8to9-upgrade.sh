0de12205e92ca3561141ca04c4f3c2e039ba8763
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-06-21 21:49:41 +0530

    Create rhel8to9-upgrade.sh

---
+++ rhel8to9-upgrade.sh
@@ -0,0 +1,15 @@
+cat /etc/redhat-release
+sudo rm -rf /root/tmp_leapp_py3
+sudo subscription-manager list --installed
+sudo subscription-manager repos --enable rhel-8-for-x86_64-baseos-rpms --enable rhel-8-for-x86_64-appstream-rpms
+sudo subscription-manager release --set 8.6
+sudo dnf update -y
+sudo dnf install leapp-upgrade -y
+sudo dnf versionlock clear
+sudo sed -i 's|.*AllowZoneDrifting*|#&|g' /etc/firewalld/firewalld.conf
+cat /etc/firewalld/firewalld.conf | grep AllowZoneDrifting
+sudo leapp preupgrade --target 9.0
+sudo leapp upgrade --target 9.0
+cat /etc/redhat-release
+uname -a
+sudo subscription-manager list --installed
