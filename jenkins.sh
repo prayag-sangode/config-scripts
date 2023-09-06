a043eb360cb8e86dda3ea590855ae7c1f71248bc
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-09-06 10:52:19 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,8 @@
-# Add required dependencies for the jenkins package
-sudo apt-get update
-sudo dnf install java-17-openjdk -y
-sudo systemctl daemon-reload
+sudo apt update
+sudo apt install openjdk-17-jre -y
+java -version
 
-# Install Jenkins
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]  https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
-sudo systemctl enable jenkins
