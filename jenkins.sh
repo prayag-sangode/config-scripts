fcc87182158ebd622c387f0509dfb142ee7e8030
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-09-06 10:48:06 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,11 @@
+# Add required dependencies for the jenkins package
+sudo apt-get update
+sudo dnf install java-17-openjdk -y
+sudo systemctl daemon-reload
+
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]  https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
