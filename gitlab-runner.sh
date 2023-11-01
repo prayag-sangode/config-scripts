4f0719a27b607a89157f3007034d55659fe0b5fd
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-11-01 15:10:42 +0530

    Update gitlab-runner.sh

--- gitlab-runner.sh
+++ gitlab-runner.sh
@@ -1,7 +1,6 @@
 sudo apt-get update && apt-get -y install docker.io
 curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
 apt-get update
-apt-get -y install gitlab-runner=16.4.0 docker.io
 echo "gitlab-runner ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 echo '{"hosts": ["tcp://0.0.0.0:2375", "unix:///var/run/docker.sock"]}' | sudo tee /etc/docker/daemon.json
 sudo sed -i -e 's|^\(ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock\)|#\1\nExecStart=/usr/bin/dockerd|' /lib/systemd/system/docker.service
