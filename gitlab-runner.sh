69060dbc74cc6e6627edfaa197136d1af1e06692
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-11-01 17:03:42 +0530

    Update gitlab-runner.sh

--- gitlab-runner.sh
+++ gitlab-runner.sh
@@ -1,9 +1,11 @@
-sudo apt-get update && apt-get -y install docker.io
 curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+sudo apt-get update && apt-get -y install docker.io && apt-get -y install gitlab-runner=16.4.0 docker.io
 echo "gitlab-runner ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+cat /etc/sudoers | grep gitlab
 echo '{"hosts": ["tcp://0.0.0.0:2375", "unix:///var/run/docker.sock"]}' | sudo tee /etc/docker/daemon.json
+cat /etc/docker/daemon.json
 sudo sed -i -e 's|^\(ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock\)|#\1\nExecStart=/usr/bin/dockerd|' /lib/systemd/system/docker.service
 sudo systemctl daemon-reload
 sudo systemctl restart gitlab-runner
-export REGISTRATION_TOKEN=<replace-me>
+export REGISTRATION_TOKEN=glrt-1w-e9u-sv6GM3xgAwRGY
 sudo gitlab-runner register --url https://gitlab.devops.telekom.de/ --registration-token $REGISTRATION_TOKEN
