1f8f177198f3bac5306a8152505bb52941ec74ed
Prayag <Prayag Sangode>
2024-02-23 18:41:52 +0000

    update istio script

---
+++ istio-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Download and extract Istio
+curl -L https://istio.io/downloadIstio | sh -
+ISTIO_VERSION="istio-1.20.3"
+ISTIO_DIR="/home/prayag/config-scripts/$ISTIO_VERSION"
+
+# Add istioctl to PATH
+export PATH="$PATH:$ISTIO_DIR/bin"
+
+# Begin pre-installation check
+istioctl x precheck
+
+# Apply Istio to the Kubernetes cluster
+istioctl install --set profile=default
+
+# Verify Istio installation
+kubectl get pods -n istio-system
+
