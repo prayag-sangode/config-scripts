fb77b75bf886c5744bc690544f714876ab9806e2
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-04-12 16:56:43 +0530

    Update mongosh.sh

--- mongosh.sh
+++ mongosh.sh
@@ -10,13 +10,13 @@ tar -zxvf mongosh-2.1.4-linux-x64.tgz
 chmod +x mongosh-2.1.4-linux-x64/bin/mongosh
 
 # Move the mongosh binary to /usr/local/bin/
-sudo cp mongosh-2.1.4-linux-x64/bin/mongosh /usr/local/bin/
+cp mongosh-2.1.4-linux-x64/bin/mongosh /usr/local/bin/
 
 # Move the mongosh_crypt_v1.so library to /usr/local/lib/
-sudo cp mongosh-2.1.4-linux-x64/bin/mongosh_crypt_v1.so /usr/local/lib/
+cp mongosh-2.1.4-linux-x64/bin/mongosh_crypt_v1.so /usr/local/lib/
 
 # Create a symbolic link to the mongosh binary in /usr/local/bin/
-sudo ln -s /usr/local/bin/mongosh-2.1.4-linux-x64/bin/* /usr/local/bin/
+ln -s /usr/local/bin/mongosh-2.1.4-linux-x64/bin/* /usr/local/bin/
 
 # Clean up
 rm -rf mongosh-2.1.4-linux-x64.tgz mongosh-2.1.4-linux-x64
