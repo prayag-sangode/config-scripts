9c10d197f54896f9fad6857ce4b726a792ebc6dc
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-06-15 17:23:34 +0530

    Create minikube-uninstall.sh

---
+++ minikube-uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Stop and delete Minikube cluster
+minikube stop
+minikube delete
+
+# Remove Minikube binary
+sudo rm -rf $(which minikube)
+
+# Remove Minikube configuration and data directories
+rm -rf ~/.minikube
+
+# Optional: Remove the Minikube context from kubectl config
+kubectl config delete-context minikube
+kubectl config delete-cluster minikube
+
+echo "Minikube has been completely uninstalled."
+prayag@minikube:~/scripts$
