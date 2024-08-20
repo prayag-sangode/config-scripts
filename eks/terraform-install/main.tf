fa888a1d7e33e1975c3c67c86ce129113193b20b
Prayag Sangode <prayag.rhce@gmail.com>
2024-08-20 05:56:52 +0000

    update

--- eks/terraform-install/main.tf
+++ eks/terraform-install/main.tf
@@ -62,24 +62,24 @@ module "eks" {
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      name = "standard-workers"
 
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
+      max_size     = 1
       desired_size = 1
     }
+
+#    two = {
+#      name = "node-group-2"
+#
+#      instance_types = ["t3.small"]
+#
+#      min_size     = 1
+#      max_size     = 2
+#      desired_size = 1
+#    }
   }
 }
 
